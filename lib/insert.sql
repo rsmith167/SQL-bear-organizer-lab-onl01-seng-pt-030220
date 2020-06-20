3046aad32c66ac2e0e194f5eebff48bd7984ee6c
Rwanda S <rwanda.ace@gmail.com>
2020-06-20 13:22:54 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 2, "F", "blue", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Rowdy", 4, "F", "orange", "sad", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Tabitha", 1, "M", "orangen", "angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Sargeant Brown", 8, "F", "blue", "sad", 10);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Melissa", 10, "M", "orange", "sad", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Grinch", 12, "F", "blue", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Wendy", 14, "M", "orange", "sad", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+(null, 16, "F", "blue", "angry", 1);
